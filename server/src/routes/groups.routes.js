77b3295f48fb69f38329f93e6132c6c5b5719186
Sophie McBroom <sophiemcbroom@Sophies-MacBook-Pro.local>
2026-02-07 21:23:56 -0500

    adding GET mygroup so front end can autoresume sessions

--- server/src/routes/groups.routes.js
+++ server/src/routes/groups.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createGroup, joinGroup } = require("../controllers/groups.controller");
+const { createGroup, joinGroup, getMyGroup } = require("../controllers/groups.controller");
 
 const router = express.Router();
 
 router.post("/", createGroup);       // POST /groups
 router.post("/join", joinGroup);     // POST /groups/join
+router.get("/me", getMyGroup);
 
 module.exports = router;
