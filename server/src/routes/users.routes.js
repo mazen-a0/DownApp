6b296368aceb3e3774f5c90723c2e530a8926896
Sophie McBroom <sophiemcbroom@Sophies-MacBook-Pro.local>
2026-02-07 20:32:27 -0500

    accept users/upsert

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -4,5 +4,6 @@ const { createUser } = require("../controllers/users.controller");
 const router = express.Router();
 
 router.post("/", createUser);
+router.post("/upsert", createUser); 
 
 module.exports = router;
