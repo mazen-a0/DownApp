289fab064c8e4b4334c4fb3bb7effabc85c34f0d
Sophie McBroom <sophiemcbroom@Sophies-MacBook-Pro.local>
2026-02-07 19:49:59 -0500

    fixing stuff

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pushToken: String,
+  pushToken: { type: String, default: null },
   groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
-  deviceID: { type: String, required: true, unique: true },
+  deviceId: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now }
 });
 
