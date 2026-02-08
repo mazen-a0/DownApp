c0fde60bf975a9ddd540abb1f560e1ecb27f1802
Sophie McBroom <sophiemcbroom@Sophies-MacBook-Pro.local>
2026-02-07 22:12:50 -0500

    Add GET /users/me/here route

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { createUser, getMyHere } = require("../controllers/users.controller");
+const requireUser = require("../middleware/requireUser");
 
 const router = express.Router();
 
+// Public: bootstrap/upsert user by deviceId
 router.post("/", createUser);
-router.post("/upsert", createUser); 
+router.post("/upsert", createUser);
 
-router.get("/me/here", getMyHere);
+// Protected: requires x-user-id
+router.get("/me/here", requireUser, getMyHere);
 
 module.exports = router;
