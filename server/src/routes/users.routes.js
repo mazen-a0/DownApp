ac09bb33e215aa8394396ee359d2f2d6108d8f3a
Sophie McBroom <sophiemcbroom@Sophies-MacBook-Pro.local>
2026-02-07 21:52:36 -0500

    add users/me/here? to user routes

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createUser } = require("../controllers/users.controller");
+const { createUser, getMyHere } = require("../controllers/users.controller");
 
 const router = express.Router();
 
 router.post("/", createUser);
 router.post("/upsert", createUser); 
+
+router.get("/me/here", getMyHere);
 
 module.exports = router;
